739bde2a5840a5f62fd204a2988ad4ebc09921ec
Damian <skrzyniarz.damian@gmail.com>
2023-12-10 15:56:56 +0100

    initial commit

---
+++ src/main/java/eu/horyzont/gradebook/GradeBookApplication.java
@@ -0,0 +1,13 @@
+package eu.horyzont.gradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/horyzont/gradebook/GradeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.horyzont.gradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
