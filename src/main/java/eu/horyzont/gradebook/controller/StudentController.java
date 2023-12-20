3630cf83c834d02c407fd481ebc0001b7ca3f896
Damian <skrzyniarz.damian@gmail.com>
2023-12-20 17:56:03 +0100

    create controller methods for student

--- src/main/java/eu/horyzont/gradebook/controller/StudentController.java
+++ src/main/java/eu/horyzont/gradebook/controller/StudentController.java
@@ -3,8 +3,7 @@ package eu.horyzont.gradebook.controller;
 import eu.horyzont.gradebook.entity.Student;
 import eu.horyzont.gradebook.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,4 +17,29 @@ public class StudentController {
     public List<Student> all(){
         return repository.findAll();
     }
+    @GetMapping("/students/id/{id}")
+    public Student studentById(@PathVariable int id){
+        return repository.findById(id).get();
+    }
+
+    @GetMapping("/students/index/{index}")
+    public Student studentByIndex(@PathVariable int index){
+        return repository.findByIndex(index);
+    }
+
+    @GetMapping("/students/major/{major}")
+    public List<Student> studentsByMajor(@PathVariable String major){
+        return repository.findAllByMajor(major);
+    }
+    @GetMapping("/students/semester/{semester}")
+    public List<Student> studentsBySemester(@PathVariable String semester){
+        return repository.findAllByMajor(semester);
+    }
+
+    @GetMapping("/students/search/{phrase}")
+    public List<Student> searchStudents(@PathVariable String phrase){
+        return repository.findByFirstNameOrLastNameContainingIgnoreCase(phrase, phrase);
+    }
+    
+
 }
--- src/main/java/eu/horyzont/gradebook/repository/StudentRepository.java
+++ src/main/java/eu/horyzont/gradebook/repository/StudentRepository.java
@@ -4,6 +4,13 @@ import eu.horyzont.gradebook.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    public Student findByIndex(int index);
+
+    List<Student> findAllByMajor(String major);
+
+    List<Student> findByFirstNameOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }
