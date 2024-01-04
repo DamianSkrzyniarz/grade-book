e4a7791adfef8d533b9b1f89b7f4e3a44530c48e
Asia <asia.troszok97@gmail.com>
2024-01-04 17:34:53 +0100

    create controller methods for teacher

--- src/main/java/eu/horyzont/gradebook/controller/TeacherController.java
+++ src/main/java/eu/horyzont/gradebook/controller/TeacherController.java
@@ -1,9 +1,11 @@
 package eu.horyzont.gradebook.controller;
 
+import eu.horyzont.gradebook.entity.Student;
 import eu.horyzont.gradebook.entity.Teacher;
 import eu.horyzont.gradebook.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,4 +20,15 @@ public class TeacherController {
     public List<Teacher> all() {
         return repository.findAll();
     }
+
+    @GetMapping("/teachers/id/{id}")
+    public Teacher teacherById(@PathVariable int id){
+        return repository.findById(id).get();
+    }
+
+    @GetMapping("/teachers/index/{index}")
+    public Teacher teacherByIndex(@PathVariable int index) { return repository.findByIndex(index);
+    }
+
+
 }
--- src/main/java/eu/horyzont/gradebook/repository/TeacherRepository.java
+++ src/main/java/eu/horyzont/gradebook/repository/TeacherRepository.java
@@ -1,10 +1,18 @@
 package eu.horyzont.gradebook.repository;
 
 
+import eu.horyzont.gradebook.entity.Student;
 import eu.horyzont.gradebook.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+
+    public Teacher
+    findByIndex(int index);
+
+    List<Teacher> findByFirstNameOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }
