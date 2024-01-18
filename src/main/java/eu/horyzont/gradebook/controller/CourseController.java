4f9877196a306f0da58839c52cd29b4ae5150abb
Damian <skrzyniarz.damian@gmail.com>
2024-01-18 14:07:30 +0100

    allow CORS

--- src/main/java/eu/horyzont/gradebook/controller/CourseController.java
+++ src/main/java/eu/horyzont/gradebook/controller/CourseController.java
@@ -3,11 +3,13 @@ package eu.horyzont.gradebook.controller;
 import eu.horyzont.gradebook.entity.Course;
 import eu.horyzont.gradebook.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 public class CourseController {
     @Autowired
--- src/main/java/eu/horyzont/gradebook/controller/TeacherController.java
+++ src/main/java/eu/horyzont/gradebook/controller/TeacherController.java
@@ -4,12 +4,14 @@ import eu.horyzont.gradebook.entity.Student;
 import eu.horyzont.gradebook.entity.Teacher;
 import eu.horyzont.gradebook.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 public class TeacherController {
 
